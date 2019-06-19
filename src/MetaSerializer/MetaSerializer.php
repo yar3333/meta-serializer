90321cde112dd597de6de874ea6ace08ed4ce826
Yaroslav Sivakov <yar3333@gmail.com>
2019-06-19 10:33:30 +0300

    v2.1.0 (check for recursion)

--- src/MetaSerializer/MetaSerializer.php
+++ src/MetaSerializer/MetaSerializer.php
@@ -19,16 +19,16 @@ class MetaSerializer
      * @param string $property
      * @throws \ReflectionException
      */
-    private function serializePropertyViaMethod($src, array &$dest, string $property) : void
+    private function serializePropertyViaMethod($src, array &$dest, string $property, \SplObjectStorage $usedObjects) : void
     {
         $method = $property . $this->methodSuffix;
         if (method_exists($src, $method)) {
             $m = new \ReflectionMethod($src, $method);
             $m->setAccessible(true);
-            $m->invokeArgs($src, [ &$dest, $property, $this ]);
+            $m->invokeArgs($src, [ &$dest, $property, $this, $usedObjects ]);
         }
         else {
-            $this->serializeProperty($src, $dest, $property);
+            $this->serializeProperty($src, $dest, $property, $usedObjects);
         }
     }
 
@@ -38,8 +38,10 @@ class MetaSerializer
      * @param string $property
      * @throws \ReflectionException
      */
-    private function serializeProperty($src, array &$dest, string $property) : void
+    private function serializeProperty($src, array &$dest, string $property, \SplObjectStorage $usedObjects=null) : void
     {
+        if ($usedObjects === null) $usedObjects = new \SplObjectStorage();
+
         $p = new \ReflectionProperty($src, $property);
 
         $ignoreNull = false;
@@ -60,27 +62,29 @@ class MetaSerializer
 
         if ($ignoreNull && $src->$property === null) return;
 
-        $dest[$renameTo ?? $property] = $this->serializeValue($src->$property);
+        $dest[$renameTo ?? $property] = $this->serializeValue($src->$property, $usedObjects);
     }
 
     /**
      * @param mixed $value
      * @return mixed
      */
-    public function serializeValue($value)
+    public function serializeValue($value, \SplObjectStorage $usedObjects=null)
     {
-    	if (is_array($value) || $value instanceof \ArrayObject)
+        if ($usedObjects === null) $usedObjects = new \SplObjectStorage();
+
+        if (is_array($value) || $value instanceof \ArrayObject)
         {
             $r = [];
             foreach ($value as $k => $v) {
-                $r[$k] = $this->serializeValue($v);
+                $r[$k] = $this->serializeValue($v, $usedObjects);
             }
             return $r;
         }
 
     	if (is_object($value))
         {
-            return $this->serializeObject($value);
+            return $this->serializeObject($value, null, $usedObjects);
         }
 
     	return $value;
@@ -92,14 +96,23 @@ class MetaSerializer
      * @return array
      * @throws \ReflectionException
      */
-    public function serializeObject($obj, array $properties=null) : array
+    public function serializeObject($obj, array $properties=null, \SplObjectStorage $usedObjects=null) : array
     {
+        if ($usedObjects === null) $usedObjects = new \SplObjectStorage();
+        if ($usedObjects->contains($obj)) return $this->onRecursiveObjectReferenceDetected($obj);
+        $usedObjects->attach($obj);
+
         if ($properties === null) $properties = array_keys(get_object_vars($obj));
 
         $r = [];
         foreach ($properties as $k) {
-            $this->serializePropertyViaMethod($obj, $r, $k);
+            $this->serializePropertyViaMethod($obj, $r, $k, $usedObjects);
         }
         return $r;
+    }
+
+    protected function onRecursiveObjectReferenceDetected($value)
+    {
+        throw new MetaSerializerException("Recursive object reference detected for value type `" . get_class($value) . "`");
     }
 }
---
+++ src/MetaSerializer/MetaSerializerException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace MetaSerializer;
+
+class MetaSerializerException extends \Exception
+{
+}
